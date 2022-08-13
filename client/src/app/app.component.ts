5e1488c5d64764c4d1a84af29a2d2aa7151bbf7d
admiralthans <73397898+admiralthans@users.noreply.github.com>
2022-08-13 21:31:21 +0800

    Checkout and Basket Info

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 import { IProduct } from './shared/models/product';
 
 @Component({
@@ -9,6 +10,18 @@ import { IProduct } from './shared/models/product';
 export class AppComponent implements OnInit {
   title = 'Skinet';
   products: IProduct[];
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(private basketService: BasketService) {}
+  ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(
+        () => {
+          console.log('initialised basket');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
 }
