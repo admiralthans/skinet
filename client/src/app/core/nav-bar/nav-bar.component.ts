6a0dfcbd4f5670ecf45ff7ca34f091080b78a3f0
admiralthans <73397898+admiralthans@users.noreply.github.com>
2022-08-20 03:54:21 +0800

    Registration and login Identity

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AccountService } from 'src/app/account/account.service';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket } from 'src/app/shared/models/basket';
+import { IUser } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
   basket$: Observable<IBasket>;
-  
-  constructor(private basketService: BasketService) { }
+  currentUser$: Observable<IUser>;
+
+  constructor(
+    private basketService: BasketService,
+    private accountServices: AccountService
+  ) {}
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
+    this.currentUser$ = this.accountServices.currentUser$;
   }
 
+  logout(){
+    this.accountServices.logout();
+  }
 }
